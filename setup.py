375113aa0af5824e2d14e41892b61756b1d7a45a
Shengyu Zhang <i@silverrainz.me>
2022-05-06 12:41:47 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
 
 setup(
     name='sphinxnotes-markdown-builder',
-    version='0.5.5',
+    version='0.5.6',
     description='sphinx builder that outputs markdown files' + ', an active fork of clayrisser/sphinx-markdown-builder',
     long_description=long_description,
     long_description_content_type='text/markdown',
